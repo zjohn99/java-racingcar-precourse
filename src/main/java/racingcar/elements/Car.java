120f89b4accee7300877edc32f313f87ae69ec63
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-11 19:39:36 +0900

    refactor(elements): Change Car constructor to private and more

--- src/main/java/racingcar/elements/Car.java
+++ src/main/java/racingcar/elements/Car.java
@@ -1,18 +1,29 @@
 package racingcar.elements;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
 
     private static final int moveDistancePerOrder = 1;
+    private static final int carNameMaximumLength = 5;
 
     private final String name;
     private int position = 0;
 
-    public Car(String name) {
+    private Car(String name) {
+        validateNameLength(name);
         this.name = name;
     }
 
-    public void moveCar() {
-        this.position += moveDistancePerOrder;
+    public static List<Car> createCarListByStringArray(String[] carNames) {
+        List<Car> carList = new ArrayList<>();
+
+        for (String carName : carNames) {
+            carList.add(new Car(carName));
+        }
+
+        return carList;
     }
 
     public String getName() {
@@ -21,5 +32,15 @@ public class Car {
 
     public int getPosition() {
         return this.position;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > carNameMaximumLength) {
+            throw new IllegalArgumentException("[ERROR]: 각 차량의 길이가 5를 초과하면 허용하지 않습니다.");
+        }
+    }
+
+    public void moveCar() {
+        this.position += moveDistancePerOrder;
     }
 }
