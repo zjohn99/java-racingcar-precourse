b636174166eb67b26c67d7da8ede7416f278edff
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-11 16:14:42 +0900

    feat(utils): InputUtils class

---
+++ src/main/java/utils/InputUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+import java.util.Scanner;
+
+public class InputUtils {
+    public static String[] inputStringArray(Scanner sc) {
+        return sc.nextLine().split(",");
+    }
+
+    public static Integer inputInteger(Scanner sc) {
+        return sc.nextInt();
+    }
+}
