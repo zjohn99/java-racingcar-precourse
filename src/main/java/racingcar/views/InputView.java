5aac2362fb1950e06befe0ca76324a0655ce4e21
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-14 01:07:02 +0900

    refactor: InputView - Change logics for inputView

--- src/main/java/racingcar/views/InputView.java
+++ src/main/java/racingcar/views/InputView.java
@@ -8,30 +8,21 @@ public class InputView {
     }
 
     public static String[] inputCarList(Scanner sc) {
-        String[] carList;
-
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
         try {
-            carList = sc.nextLine().split(",");
+            return sc.nextLine().split(",");
         } catch (Exception e) {
             throw new IllegalArgumentException("[ERROR]: 경주 자동차 정보의 입력을 확인하시기 바랍니다.");
         }
-
-        return carList;
     }
 
     public static int inputTurn(Scanner sc) {
-        int turn;
-
         System.out.println("시도할 회수는 몇회인가요?");
 
         try {
-            turn = sc.nextInt();
+            return sc.nextInt();
         } catch (Exception e) {
             throw new IllegalArgumentException("[ERROR]: 정수만 입력할 수 있습니다.");
         }
-
-        return turn;
     }
 }
